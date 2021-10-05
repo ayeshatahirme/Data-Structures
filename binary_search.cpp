953849af86a0a6b50df490cc80779ccc5497174d
pravigya <77122170+pravigya@users.noreply.github.com>
2021-10-06 00:32:42 +0530

    Added Driver Code to binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -23,3 +23,14 @@ int binarySearch(int arr[], int l, int r, int num)
 	// If element is not present return -1
 	return -1;
 }
+# Driver Code
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+  
+# Function call
+result = binarySearch(arr, 0, len(arr)-1, x)
+  
+if result != -1:
+    print ("Element is present at index % d" % result)
+else:
+    print ("Element is not present in array")
