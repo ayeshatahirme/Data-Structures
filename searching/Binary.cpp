c950aac031f65515a1babd52fa2168734dba42a0
ABAbdulah <89691370+ABAbdulah@users.noreply.github.com>
2021-10-09 14:14:24 +0500

    Commented

--- searching/Binary.cpp
+++ searching/Binary.cpp
@@ -7,23 +7,24 @@ int binarySearch(int arr[], int num, int low, int high)
 	int mid;
 	if (low > high)
 	{
-		return -1;										// If low is greater than high it returns -1
+		return -1;									// If low is greater than high it returns -1
 	}
 	mid = (low + high) / 2;
 	if (num == arr[mid])									// If the number and mid element of array are same 
-	{													// it returns mid index.
+	{											// it returns mid index.
 		return mid;
 	}
 	if (num < arr[mid])									// If the number is less than mid element than high 
-	{													// is set at mid.
+	{											// is set at mid.
 		return binarySearch(arr, num, low, mid - 1);
 	}
 	else
-	{													// If the number is greater than mid element than low 
+	{											// If the number is greater than mid element than low 
 		return binarySearch(arr, num, mid + 1, high);		// is set at mid
 	}
 }
 
+/* Driver Code */
 int main()
 {
 	int arr[] = { 2, 3, 4, 10, 40 };
